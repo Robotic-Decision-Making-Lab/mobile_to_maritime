65cc96fb11c26bee98151e557f8711b621e2b665
Evan Palmer <evanp922@gmail.com>
2024-09-17 21:52:41 -0700

    Continued impl

--- message_transforms/src/transforms.cpp
+++ message_transforms/src/transforms.cpp
@@ -44,27 +44,52 @@ auto MessageTransforms::on_configure(const rclcpp_lifecycle::State & /*state*/) 
   for (const auto & topic : params_.incoming_topics) {
     std::vector<rclcpp::TopicEndpointInfo> info = get_publishers_info_by_topic(topic);
 
-    // TODO(evan-palmer): figure out how to reason about multiple publishers
-    for (const auto & endpoint : info) {
-      if (endpoint.endpoint_type() == rclcpp::EndpointType::Publisher) {
-        auto qos = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(endpoint.qos_profile()));
+    // Filter the endpoints to include only publishers from other nodes
+    const auto endpoint_it = std::ranges::find_if(info, [this](const auto & endpoint) {
+      return endpoint.endpoint_type() == rclcpp::EndpointType::Publisher &&
+             (endpoint.node_name() != get_name() || endpoint.node_namespace() != get_namespace());
+    });
 
-        // TODO(evan-palmer): figure out how to get the message type from the endpoint
-        publishers_.push_back(
-          create_publisher<geometry_msgs::msg::Pose>(params_.outgoing_messages[topic].outgoing_topic, qos));
+    // Set the QoS policy to system defaults if no publishers are found for the topic
+    rclcpp::QoS qos;
+    if (endpoint_it == info.end()) {
+      RCLCPP_WARN(get_logger(), "No publishers found for topic %s. Setting QoS policy to system defaults", topic);
+      qos = rclcpp::SystemDefaultsQoS();
+    } else {
+      qos = endpoint_it->qos_profile();
 
-        auto sub = create_subscription<geometry_msgs::msg::Pose>(
-          topic, qos, [this, topic](const std::shared_ptr<geometry_msgs::msg::Pose> message) {
-            transform_message(message);
-            publishers_.back()->publish(*message);
-          };);
-
-        subscriptions_.push_back(sub);
+      // The history policy can be set to unknown when the QoS profile is copied. See the following for more
+      // information:
+      // https://github.com/foxglove/ros-foxglove-bridge/blob/26d5fac4d11b34edc583ff63236910c94f4416e7/ros2_foxglove_bridge/src/ros2_foxglove_bridge.cpp#L657
+      if (qos.history() == rclcpp::HistoryPolicy::Unknown) {
+        qos.history(rclcpp::HistoryPolicy::SystemDefault);
       }
     }
-  }
 
-  return CallbackReturn::SUCCESS;
+    // Try this:
+    // https://stackoverflow.com/questions/582331/is-there-a-way-to-instantiate-objects-from-a-string-holding-their-class-name
+
+    // // TODO(evan-palmer): figure out how to reason about multiple publishers
+    // for (const auto & endpoint : info) {
+    //   if (endpoint.endpoint_type() == rclcpp::EndpointType::Publisher) {
+    //     auto qos = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(endpoint.qos_profile()));
+
+    //     // TODO(evan-palmer): figure out how to get the message type from the endpoint
+    //     // publishers_.push_back(
+    //     //   create_publisher<geometry_msgs::msg::Pose>(params_.outgoing_messages[topic].outgoing_topic, qos));
+
+    //     // auto sub = create_subscription<geometry_msgs::msg::Pose>(
+    //     //   topic, qos, [this, topic](const std::shared_ptr<geometry_msgs::msg::Pose> message) {
+    //     //     transform_message(message);
+    //     //     publishers_.back()->publish(*message);
+    //     //   };);
+
+    //     // subscriptions_.push_back(sub);
+    //   }
+  }
+}
+
+return CallbackReturn::SUCCESS;
 }
 
 auto MessageTransforms::on_activate(const rclcpp_lifecycle::State & /*state*/) -> CallbackReturn
